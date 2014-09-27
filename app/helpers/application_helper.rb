47b7b46b0b59a0ec8ee5d210139f7cefe446d695
Piotr Pupczyk <piotrek@jakbyco.com>
2014-09-27 10:09:22 +0200

    add helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+def publication_status(article)
+if article.published?
+"Published at #{article.updated_at
+.strftime('%A, %B %e')}"
+else
+"Unpublished"
+end
+end
+
 end
